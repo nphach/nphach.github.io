0d02a427fea2c3016aa0e7a049900aef43f9d25c
Nikki Phach <nikkiphach@gmail.com>
2024-06-18 16:26:30 -0400

    Revert "Moves to shadcn"
    This reverts commit a0e28b50cd2057cd5393a778fe64722233176991.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# My personal portfolio
+My personal portfolio
 
 To-do:
-- add header
 - add about me section
 - add projects section
 - add contact me section
+- switch out header image for cool AI image
 
 Bugs/ issues:
-- links should collapse to dropdown menu for smaller screens
+- expanded drop-down menu is transparent when not scrolled (make a state for expanded)
+- drop down links are centered, social buttons are right alligned
---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import {useState, useEffect} from "react";
+import {Nav, Navbar, Container} from "react-bootstrap";
+import {List} from "react-bootstrap-icons";
+import logo from "../assets/img/logo.png";
+import githubLogo from "../assets/img/githubLogo.png";
+import linkedinLogo from "../assets/img/linkedinLogo.png";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+  
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <List />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === "home" ? "active-navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>home</Nav.Link>
+            <Nav.Link href="#about" className={activeLink === "about" ? "active-navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("about")}>about</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === "projects" ? "active-navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+                <a href="https://www.github.com/nphach/"><img src={githubLogo} alt="" /></a>
+                <a href="https://www.linkedin.com/in/nphach/"><img src={linkedinLogo} alt="" /></a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
